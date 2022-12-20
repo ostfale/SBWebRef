04d9ec60f87e7d29b6a19a6e8f4de44d924548c8
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2022-12-20 07:58:33 +0100

    Initial Commit

---
+++ src/main/java/de/ostfale/ref/sbwebref/SbWebRefApplication.java
@@ -0,0 +1,13 @@
+package de.ostfale.ref.sbwebref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWebRefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbWebRefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ostfale/ref/sbwebref/SbWebRefApplicationTests.java
@@ -0,0 +1,16 @@
+package de.ostfale.ref.sbwebref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class SbWebRefApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
